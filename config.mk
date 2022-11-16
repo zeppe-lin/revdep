10a80148b9049634580ad4554ee7f50e8c81ad0b
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-11-16 18:18:30 +0200

    config.mk: restyle, add header

--- config.mk
+++ config.mk
@@ -1,17 +1,23 @@
-# revdep version
+# This file is a part of revdep.
+# See COPYING and COPYRIGHT files for corresponding information.
+
+# revdep version if undefined
 VERSION = 2.0
 
 # paths
 PREFIX = /usr/local
-BINDIR = $(PREFIX)/bin
-MANDIR = $(PREFIX)/share/man
+BINDIR = ${PREFIX}/bin
+MANDIR = ${PREFIX}/share/man
 ETCDIR = /etc
 
 # flags
 CXXFLAGS += -std=c++11 -Wall -Wextra -pedantic
-CPPFLAGS += -DVERSION=\"$(VERSION)\"
+CPPFLAGS += -DVERSION=\"${VERSION}\"
 LDFLAGS += --static $(shell pkg-config --libs --static libelf)
 
 # compiler and linker
 CXX ?= g++
-LD = $(CXX)
+LD = ${CXX}
+
+# vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
+# End of file.
