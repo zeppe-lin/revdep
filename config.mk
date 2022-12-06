0b2865f1feca46744a80013894e9d870175a59c6
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-06 05:01:27 +0200

    config.mk: minor changes

--- config.mk
+++ config.mk
@@ -1,4 +1,3 @@
-# This file is a part of revdep.
 # See COPYING and COPYRIGHT files for corresponding information.
 
 # revdep version if undefined
@@ -11,13 +10,13 @@ MANDIR = ${PREFIX}/share/man
 ETCDIR = /etc
 
 # flags
-CXXFLAGS += -std=c++11 -Wall -Wextra -pedantic
-CPPFLAGS += -DVERSION=\"${VERSION}\"
-LDFLAGS += --static $(shell pkg-config --libs --static libelf)
+CPPFLAGS = -DVERSION=\"${VERSION}\"
+CXXFLAGS = -std=c++11 -Wall -Wextra -pedantic
+LDFLAGS  = --static $(shell pkg-config --libs --static libelf)
 
 # compiler and linker
-CXX ?= g++
-LD = ${CXX}
+CXX = g++
+LD  = ${CXX}
 
 # vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
 # End of file.
