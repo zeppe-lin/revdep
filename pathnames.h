b88b03b5a8355650f5722ae7c07e842ed2898567
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-22 19:28:07 +0200

    pathnames.h: add comments for definitions

--- pathnames.h
+++ pathnames.h
@@ -2,8 +2,13 @@
 
 #pragma once
 
+/* Default location for revdep package configuration. */
 #define PATH_REVDEP_D     "/etc/revdep.d"
+
+/* Default location for the package database. */
 #define PATH_PKG_DB       "/var/lib/pkg/db"
+
+/* Default location for dynamic loader configuration. */
 #define PATH_LD_SO_CONF   "/etc/ld.so.conf"
 
 /* vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
