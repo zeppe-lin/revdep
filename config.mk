ffcae8c9fd540fdc703981c10e74946620d82446
Alexandr Savca <alexandr.savca89@gmail.com>
2023-05-17 01:14:28 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,17 +1,17 @@
 # project metadata
-NAME = revdep
-VERSION = 3.1
-DIST = ${NAME}-${VERSION}
+NAME      = revdep
+VERSION   = 3.1
+DIST      = ${NAME}-${VERSION}
 
 # paths
-PREFIX = /usr
+PREFIX    = /usr
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\"
-CXXFLAGS = -std=c++11 -pedantic -Wall -Wextra -Wconversion -Wcast-align \
-	   -Wunused -Wshadow -Wold-style-cast
-LDFLAGS  = -static $(shell pkg-config --libs --static libelf)
+CPPFLAGS  = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\"
+CXXFLAGS  = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
+	    -Wcast-align -Wunused -Wshadow -Wold-style-cast
+LDFLAGS   = -static $(shell pkg-config --libs --static libelf)
 
 # compiler and linker
 CXX = c++
