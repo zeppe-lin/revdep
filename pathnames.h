cc9d183c3efdada52cfce20a5a2f8f02551ef992
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-06 08:18:11 +0300

    pathnames.h: add (doxygen) comments

--- pathnames.h
+++ pathnames.h
@@ -1,15 +1,17 @@
-/* See COPYING and COPYRIGHT files for corresponding information. */
+//! \file  pathnames.h
+//! \brief Absolute filenames that revdep wants for various defaults.
+//         See COPYING and COPYRIGHT files for corresponding information.
 
 #pragma once
 
-/* Default location for revdep package configuration. */
+//!< Default location for revdep package configuration.
 #define PATH_REVDEP_D     "/etc/revdep.d"
 
-/* Default location for the package database. */
+//!< Default location for the package database.
 #define PATH_PKG_DB       "/var/lib/pkg/db"
 
-/* Default location for dynamic loader configuration. */
+//!< Default location for dynamic loader configuration.
 #define PATH_LD_SO_CONF   "/etc/ld.so.conf"
 
-/* vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
- * End of file. */
+// vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
+// End of file.
