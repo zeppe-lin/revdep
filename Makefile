c8d11e0c8ccdcfb215fbc44b2ea6a30aba334e69
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-06 08:44:17 +0300

    add {,un}install-bashcomp targets to make(1p)
    Meanwhile, indent fix.

--- Makefile
+++ Makefile
@@ -25,9 +25,16 @@ install: all
 	chmod 0755     ${DESTDIR}${PREFIX}/bin/revdep
 	chmod 0644     ${DESTDIR}${MANPREFIX}/man1/revdep.1
 
+install-bashcomp:
+	mkdir -p ${DESTDIR}${BASHCOMPDIR}
+	cp -f bash_completion ${DESTDIR}${BASHCOMPDIR}/revdep
+
 uninstall:
 	rm -f ${DESTDIR}${PREFIX}/revdep
 	rm -f ${DESTDIR}${MANPREFIX}/man1/revdep.1
+
+uninstall-bashcomp:
+	rm -f ${DESTDIR}${BASHCOMPDIR}/revdep
 
 clean:
 	rm -f ${OBJS} revdep revdep.1
@@ -36,4 +43,4 @@ clean:
 dist: clean
 	git archive --format=tar.gz -o ${DIST}.tar.gz --prefix=${DIST}/ HEAD
 
-.PHONY: all install uninstall clean dist
+.PHONY: all install install-bashcomp uninstall uninstall-bashcomp clean dist
--- config.mk
+++ config.mk
@@ -1,18 +1,19 @@
 # project metadata
-NAME      = revdep
-VERSION   = 3.1
-DIST      = ${NAME}-${VERSION}
+NAME         = revdep
+VERSION      = 3.1
+DIST         = ${NAME}-${VERSION}
 
 # paths
-PREFIX    = /usr
-MANPREFIX = ${PREFIX}/share/man
+PREFIX       = /usr
+MANPREFIX    = ${PREFIX}/share/man
+BASHCOMPDIR  = ${PREFIX}/share/bash-completion/completions
 
 # flags
-CPPFLAGS  = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\"
-CXXFLAGS  = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
-	    -Wcast-align -Wunused -Wshadow -Wold-style-cast
-LDFLAGS   = -static $(shell pkg-config --libs --static libelf)
+CPPFLAGS     = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\"
+CXXFLAGS     = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
+	       -Wcast-align -Wunused -Wshadow -Wold-style-cast
+LDFLAGS      = -static $(shell pkg-config --libs --static libelf)
 
 # compiler and linker
-CXX = c++
-LD  = ${CXX}
+CXX          = c++
+LD           = ${CXX}
