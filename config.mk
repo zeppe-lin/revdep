14c18e560080c9adeec8165c2aa1373718c21306
Alexandr Savca <alexandr.savca89@gmail.com>
2023-09-12 02:13:25 +0300

    config.mk: indent

--- config.mk
+++ config.mk
@@ -1,15 +1,15 @@
 # project metadata
-NAME         = revdep
-VERSION      = 3.1
-DIST         = ${NAME}-${VERSION}
+NAME        = revdep
+VERSION     = 3.1
+DIST        = ${NAME}-${VERSION}
 
 # paths
-PREFIX       = /usr
-MANPREFIX    = ${PREFIX}/share/man
-BASHCOMPDIR  = ${PREFIX}/share/bash-completion/completions
+PREFIX      = /usr
+MANPREFIX   = ${PREFIX}/share/man
+BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 
 # flags
-CPPFLAGS     = -DVERSION=\"${VERSION}\"
-CXXFLAGS     = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
-	       -Wcast-align -Wunused -Wshadow -Wold-style-cast
-LDFLAGS      = -static $(shell pkg-config --libs --static libelf)
+CPPFLAGS    = -DVERSION=\"${VERSION}\"
+CXXFLAGS    = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
+	      -Wcast-align -Wunused -Wshadow -Wold-style-cast
+LDFLAGS     = -static $(shell pkg-config --libs --static libelf)
