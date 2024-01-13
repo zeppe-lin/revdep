84b01381b5ed6a8a4b7a7bcae36eec582416ef92
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-13 14:12:26 +0200

    config.mk: update a bit debug cxxflags

--- config.mk
+++ config.mk
@@ -11,7 +11,7 @@ BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 # flags
 CPPFLAGS    = -DVERSION=\"${VERSION}\"
 ifeq (${DEBUG}, 1)
-CXXFLAGS    = -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
+CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
               -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
 LDFLAGS     = $(shell pkg-config --libs libelf) ${CXXFLAGS} -lasan -lubsan
 else
