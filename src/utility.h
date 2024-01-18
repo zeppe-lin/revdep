558b090d2b65149c709bad04ba3ea3665cdaa917
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-18 18:33:40 +0200

    utility.h: indent a bit

--- src/utility.h
+++ src/utility.h
@@ -17,24 +17,20 @@ typedef vector<string> StringVector;
 //! \tparam  in         string to split
 //! \tparam  out        vector of strings that will contain the result
 //! \tparam  delimiter  delimiter
-void
-split(const string &in, StringVector &out, char delimiter);
+void split(const string &in, StringVector &out, char delimiter);
 
 //! TODO document
-void
-ReadRdConf(const string &path, StringVector &dirs);
+void ReadRdConf(const string &path, StringVector &dirs);
 
 //! TODO document
-bool
-ReadLdConf(const string &path, StringVector &dirs, int maxDepth);
+bool ReadLdConf(const string &path, StringVector &dirs, int maxDepth);
 
 //! \brief   Test for a regular file
 //!
 //! \tparam  path       file path
 //!
 //! \return  \a true if \a path is a regular file, \a false otherwise
-bool
-IsRegularFile(const string &path);
+bool IsRegularFile(const string &path);
 
 // vim: sw=2 ts=2 sts=2 et cc=72 tw=70
 // End of file.
