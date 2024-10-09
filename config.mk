02d3bc415eb0c8fda7ce6f5db625202dfb8c6dab
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-09 17:05:23 +0300

    config.mk: DEBUG=y for debug build, place release flags first

--- config.mk
+++ config.mk
@@ -9,13 +9,14 @@ MANPREFIX   = ${PREFIX}/share/man
 BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 
 # flags
+ifneq (${DEBUG}, y)
 CPPFLAGS    = -DVERSION=\"${VERSION}\"
-ifeq (${DEBUG}, 1)
+CXXFLAGS    = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
+              -Wcast-align -Wunused -Wshadow -Wold-style-cast
+LDFLAGS     = -static $(shell pkg-config --libs --static libelf)
+else
+CPPFLAGS    = -DVERSION=\"${VERSION}\"
 CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
               -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
 LDFLAGS     = $(shell pkg-config --libs libelf) ${CXXFLAGS} -lasan -lubsan
-else
-CXXFLAGS    = -std=c++11 -pedantic -Wall -Wextra -Wconversion \
-	      -Wcast-align -Wunused -Wshadow -Wold-style-cast
-LDFLAGS     = -static $(shell pkg-config --libs --static libelf)
 endif
