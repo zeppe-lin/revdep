9bdb7a0dfa0dc1cd6bffa40e64d521e909f44940
Alexandr Savca <alexandr.savca89@gmail.com>
2025-03-07 18:01:45 +0200

    document pkg.{cpp,h}

--- src/pkg.cpp
+++ src/pkg.cpp
@@ -1,109 +1,170 @@
 //! \file  pkg.cpp
 //! \brief Package class implementation.
-//!        See COPYING and COPYRIGHT files for corresponding information.
+//!
+//! This file implements the `Package` class and related functions for
+//! reading package information from a database and associating
+//! directory configurations with packages.
+//!
+//! See COPYING and COPYRIGHT files for corresponding information.
 
-#include <algorithm>
-#include <fstream>
+#include <algorithm>  // For std::find
+#include <fstream>    // For std::ifstream
 
-#include <dirent.h>
+#include <dirent.h>   // For DIR, opendir, readdir, closedir
 
 #include "pkg.h"
 
 using namespace std;
 
+/*!
+ * \brief Constructor for the Package class.
+ *
+ * Initializes a Package object with name, version, and file list.
+ * Directory list is initialized as empty.
+ *
+ * \param name    Package name.
+ * \param version Package version.
+ * \param files   Vector of file paths belonging to the package.
+ */
 Package::Package(const string &name, const string &version,
     const StringVector &files)
   : _name(name),
     _version(version),
     _files(files),
-    _dirs({}),
-    _ignore(false)
+    _dirs({}),      // Initialize directory list as empty
+    _ignore(false)  // Initialize ignore flag as false
 {}
 
+/*!
+ * \brief Reads package information from an input stream.
+ *
+ * Reads package name, version, and file paths from the provided input
+ * stream. The format is expected to be: package name, package
+ * version, followed by file paths each on a new line, and an empty
+ * line to separate packages.
+ *
+ * \param in Input stream to read package data from.
+ *
+ * \return A Package object populated with data from the input stream.
+ *         Returns an empty Package if fewer than 2 fields (name and
+ *         version) are read.
+ */
 static Package
 readPackage(istream &in)
 {
-  string       line;
-  size_t       fields = 0;
-  string       name;
-  string       version;
-  StringVector files;
+  string       line;        // Line read from input stream
+  size_t       fields = 0;  // Count of fields read
+  string       name;        // Package name
+  string       version;     // Package version
+  StringVector files;       // Vector of file paths
 
+  // Read until empty line or end of stream
   while (getline(in, line) && line != "")
   {
     switch (++fields)
     {
-      case 1:                 name = line ; break;
-      case 2:              version = line ; break;
-      default: files.push_back("/" + line); break;
+      case 1:                 name = line ; break; // Read package name
+      case 2:              version = line ; break; // Read package version
+      default: files.push_back("/" + line); break; // Read file path
     }
   }
 
+  // Return empty Package if name and version not found
   if (fields < 2)
     return Package("", "", files);
 
-  return Package(name, version, files);
+  return Package(name, version, files);  // Construct and return Package
 }
 
+/*!
+ * \brief Reads packages from a database file.
+ *
+ * Opens and reads the package database file specified by 'path'.
+ * Parses each package entry using readPackage and stores the Package
+ * objects in the provided PackageVector.
+ *
+ * \param path Path to the package database file.
+ * \param pkgs [out] PackageVector to store read Package objects.
+ *
+ * \return True if packages are successfully read (at least one
+ *         package), false if file cannot be opened or no packages are
+ *         read.
+ */
 bool
 ReadPackages(const string &path, PackageVector &pkgs)
 {
-  ifstream fin;
-
-  fin.open(path.c_str());
-
+  ifstream fin(path.c_str());
   if (!fin.is_open())
-    return false;
+    return false; // Return false if packages database file cannot be opened
 
+  // Read packages until end of file
   while (true)
   {
-    Package pkg = readPackage(fin);
+    Package pkg = readPackage(fin);  // Read single package
 
+    // Break loop if empty package (end of packages)
     if (pkg.Name() == "" && pkg.Version() == "")
       break;
 
-    pkgs.push_back(pkg);
+    pkgs.push_back(pkg);  // Add read package to vector
   }
 
-  fin.close();
+  fin.close();  // Close package database file
 
-  return (pkgs.size() > 0);
+  return (pkgs.size() > 0);  // Return true if packages read
 }
 
+/*!
+ * \brief Reads package-specific directories from files in a
+ *        directory.
+ *
+ * Opens the directory specified by 'path' and reads each regular file
+ * within it.  For each file, it reads directory paths using
+ * ReadRdConf and associates these directories with a Package object
+ * whose name matches the filename.
+ *
+ * \param path Path to the directory containing package directory
+ *             configuration files.
+ * \param pkgs [in/out] PackageVector to update with package-specific
+ *             directories.
+ */
 void
 ReadPackageDirs(const string &path, PackageVector &pkgs)
 {
-  DIR *dir;
+  DIR *dir;  // Directory stream
 
-  dir = opendir(path.c_str());
+  dir = opendir(path.c_str());  // Open directory
 
   if (dir == NULL)
-    return;
+    return;  // Return if directory cannot be opened
 
-  struct dirent *de;
+  struct dirent *de;  // Directory entry
 
+  // Read directory entries
   while ((de = readdir(dir)) != NULL)
   {
     if (de->d_type != DT_REG)
-      continue;
+      continue;  // Skip if not a regular file
 
-    StringVector dirs;
+    StringVector dirs;  // Vector to store directory paths
 
+    // Read directory paths from config file
     ReadRdConf(path + "/" + de->d_name, dirs);
 
     if (dirs.size() == 0)
-      continue;
+      continue;  // Skip if no directories read
 
+    // Find package by name (filename)
     PackageVector::iterator pkg =
       find(pkgs.begin(), pkgs.end(), de->d_name);
 
     if (pkg == pkgs.end())
-      continue;
+      continue;  // Skip if package not found
 
-    pkg->Dirs(dirs);
+    pkg->Dirs(dirs);  // Set directories for the package
   }
 
-  closedir(dir);
+  closedir(dir);  // Close directory stream
 }
 
 // vim: sw=2 ts=2 sts=2 et cc=72 tw=70
--- src/pkg.h
+++ src/pkg.h
@@ -1,6 +1,10 @@
 //! \file  pkg.h
 //! \brief Package class definition.
-//!        See COPYING and COPYRIGHT files for corresponding information.
+//!
+//! This file defines the `Package` class and related type definitions
+//! and function declarations for managing package information.
+//!
+//! See COPYING and COPYRIGHT files for corresponding information.
 
 #pragma once
 
@@ -8,63 +12,158 @@
 
 using namespace std;
 
+/*!
+ * \class Package
+ * \brief Represents a software package with its properties and
+ *        associated files and directories.
+ *
+ * The `Package` class stores information about a software package,
+ * including its name, version, files, and associated directories.  It
+ * provides methods to access these properties and to check package
+ * compatibility.
+ */
 class Package {
 private:
-  string        _name;
-  string        _version;
-  StringVector  _files;
-  StringVector  _dirs;
-  bool          _ignore;
+  string        _name;     //!< Package name.
+  string        _version;  //!< Package version string.
+  StringVector  _files;    //!< Vector of file paths in package.
+  StringVector  _dirs;     //!< Vector of directories for package.
+  bool          _ignore;   //!< Flag to indicate if package is ignored.
 
 public:
+  /*!
+   * \brief Constructor for the Package class.
+   *
+   * Initializes a Package object with name, version, and file list.
+   *
+   * \param name    Package name.
+   * \param version Package version.
+   * \param files   Vector of file paths for the package.
+   */
   Package(const string &name, const string &version,
       const StringVector &files);
 
+  /*!
+   * \brief Returns the name of the package.
+   *
+   * \return Package name as a constant string reference.
+   */
   const string& Name() const
   {
     return _name;
   }
 
+  /*!
+   * \brief Returns the version of the package.
+   *
+   * \return Package version as a constant string reference.
+   */
   const string& Version() const
   {
     return _version;
   }
 
+  /*!
+   * \brief Returns the list of files in the package.
+   *
+   * \return Vector of file paths as a constant StringVector
+   *         reference.
+   */
   const StringVector& Files() const
   {
     return _files;
   }
 
+  /*!
+   * \brief Returns the list of directories associated with the
+   *        package.
+   *
+   * \return Vector of directory paths as a constant StringVector
+   *         reference.
+   */
   const StringVector& Dirs() const
   {
     return _dirs;
   }
 
+  /*!
+   * \brief Sets the list of directories for the package.
+   *
+   * \param dirs Vector of directory paths to set for the package.
+   */
   void Dirs(const StringVector &dirs)
   {
     _dirs = dirs;
   }
 
+  /*!
+   * \brief Checks if the package is marked as ignored.
+   *
+   * \return True if the package is ignored, false otherwise.
+   */
   bool Ignore() const
   {
     return _ignore;
   }
 
+  /*!
+   * \brief Marks the package as ignored.
+   *
+   * Sets the ignore flag to true for this package.
+   */
   void Ignore()
   {
     _ignore = true;
   }
 
+  /*!
+   * \brief Equality operator overload for Package and string.
+   *
+   * Compares the package name with a given string.
+   *
+   * \param name String to compare with the package name.
+   *
+   * \return True if the package name is equal to the given string,
+   *         false otherwise.
+   */
   bool operator ==(const string &name) const
   {
     return _name == name;
   }
 }; /* class Package */
 
+/*!
+ * \brief Type alias for a vector of Package objects.
+ *
+ * Defines `PackageVector` as a synonym for `std::vector<Package>`.
+ */
 typedef vector <Package> PackageVector;
 
+/*!
+ * \brief Reads packages from a database file.
+ *
+ * Parses package information from the file specified by 'path' and
+ * populates a PackageVector.
+ *
+ * \param path Path to the package database file.
+ * \param pkgs [out] PackageVector to store the read Package objects.
+ *
+ * \return True if packages are read successfully, false otherwise.
+ */
 bool ReadPackages(const string &path, PackageVector &pkgs);
 
+/*!
+ * \brief Reads package-specific directories from a directory.
+ *
+ * Reads directory configurations from files in the directory
+ * specified by 'path' and associates them with corresponding packages
+ * in the provided PackageVector.
+ *
+ * \param path Path to the directory containing package directory
+ *             configuration files.
+ * \param pkgs [in/out] PackageVector to update with package
+ *             directories.
+ */
 void ReadPackageDirs(const string &path, PackageVector &pkgs);
 
 // vim: sw=2 ts=2 sts=2 et cc=72 tw=70
