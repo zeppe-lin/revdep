84008a1adc82851b8fa7d949422b985a8e12bcc9
Alexandr Savca <alexandr.savca89@gmail.com>
2026-02-26 15:33:53 +0200

    librevdep: expose new module headers via librevdep.h

--- src/librevdep/librevdep.h
+++ src/librevdep/librevdep.h
@@ -8,4 +8,11 @@
 #pragma once
 
 #include "elf_cache.h"
+#include "elf.h"
 #include "pkg.h"
+#include "utility.h"
+
+#include "revdep_context.h"
+#include "revdep_engine.h"
+#include "revdep_parallel.h"
+#include "revdep_format.h"
